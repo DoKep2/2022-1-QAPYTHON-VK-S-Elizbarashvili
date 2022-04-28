a35f805ab9b59cef03e4c11ac67ae9e36ef5754e
Sergo <sergo.elisbar@yandex.ru>
2022-04-28 23:41:10 +0300

    feat: fix tests naming

--- homework7/code/tests/test_mock_server.py
+++ homework7/code/tests/test_mock_server.py
@@ -2,24 +2,24 @@ from util.data_generator import DataGenerator
 
 
 class Test:
-    def test_get_teacher(self, mock_client, name=DataGenerator.fake_name()):
-        surname = mock_client.get_teacher_rate(name)
-        assert 'not found' in surname
+    def test_get_non_existent_teacher(self, mock_client, name=DataGenerator.fake_name()):
+        rate = mock_client.get_teacher_rate(name)
+        assert 'not found' in rate
 
     def test_add_teacher(self, mock_client, name=DataGenerator.fake_name(), rate=DataGenerator.fake_number()):
         mock_client.add_teacher_rate(name, rate)
-        a = mock_client.get_teacher_rate(name)
-        assert str(a) == rate
+        teacher_rate = mock_client.get_teacher_rate(name)
+        assert teacher_rate == rate
 
     def test_delete_teacher_rate(self, mock_client, name=DataGenerator.fake_name(), rate=DataGenerator.fake_number()):
         mock_client.add_teacher_rate(name, rate)
         mock_client.delete_teacher_rate(name)
-        a = mock_client.get_teacher_rate(name)
-        assert 'not found' in a
+        teacher_rate = mock_client.get_teacher_rate(name)
+        assert 'not found' in teacher_rate
 
     def test_update_teacher_rate(self, mock_client, name=DataGenerator.fake_name(), rate=DataGenerator.fake_number(),
                                  new_rate=DataGenerator.fake_number()):
         mock_client.add_teacher_rate(name, rate)
         mock_client.update_teacher_rate(name, new_rate)
-        a = mock_client.get_teacher_rate(name)
-        assert str(a) == new_rate
+        rate = mock_client.get_teacher_rate(name)
+        assert rate == new_rate
