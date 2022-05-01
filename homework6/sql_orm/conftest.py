a895796b6e75320cb63007ca2cbcf518d3353db2
Sergo <sergo.elisbar@yandex.ru>
2022-05-01 03:15:56 +0300

    feat: fix parallel running

--- homework6/sql_orm/conftest.py
+++ homework6/sql_orm/conftest.py
@@ -1,21 +1,24 @@
+import sys
+from os import O_CREAT
+
 import pytest
+from xdist import get_xdist_worker_id
 
 from mysql.client import MysqlClient
 from parsers.log_analyzer import LogAnalyzer
 
 
 def pytest_configure(config):
-    mysql_client = MysqlClient(db_name='TEST_SQL')
-    if not hasattr(config, 'workerinput'):
+    if str(config.workerinput["workerid"]) == "gw0":
+        mysql_client = MysqlClient(db_name='TEST_SQL')
         mysql_client.create_db()
-
-    mysql_client.connect(db_created=True)
-    mysql_client.create_table("requests_amount")
-    mysql_client.create_table("requests_amount_by_type")
-    mysql_client.create_table("most_frequent_requests")
-    mysql_client.create_table("biggest_requests_with_request_status_4XX")
-    mysql_client.create_table("top_users_by_amount_of_requests_with_status_5XX")
-    mysql_client.connection.close()
+        mysql_client.connect(db_created=True)
+        mysql_client.create_table("requests_amount")
+        mysql_client.create_table("requests_amount_by_type")
+        mysql_client.create_table("most_frequent_requests")
+        mysql_client.create_table("biggest_requests_with_request_status_4XX")
+        mysql_client.create_table("top_users_by_amount_of_requests_with_status_5XX")
+        mysql_client.connection.close()
 
 
 @pytest.fixture(scope='session')
@@ -30,4 +33,3 @@ def mysql_client(request) -> MysqlClient:
 def log_analyzer() -> LogAnalyzer:
     analyzer = LogAnalyzer()
     return analyzer
-
